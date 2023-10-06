fc8711002de40dde3cee78ed4e40de5a881667a5
Uriel Gonzalez <ugonzalez8773@gmail.com>
2023-10-05 21:51:09 -0600

    new header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <>
       <h1 className="font-black text-5xl text-center w-2/3 mx-auto">
-        Seguimiento de pacientes para {''}
+        Seguimiento de pacientes para {' '} 
         <span className="text-indigo-600">Veterinaria</span>
       </h1>
     </>
